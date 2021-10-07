c8a7f4e878fba67335e21b455b3dfec3f3a0266c
Isabella <imkenton@gmail.com>
2021-10-07 13:54:43 -0700

    added 3 files, employee.h, employeeMain.c and employeeTable.c

---
+++ employeeMain.c
@@ -0,0 +1,50 @@
+#include <string.h>
+#include <stdlib.h>
+#include "employee.h"
+//#include <stdio.h>
+int main(void){
+
+    PtrToEmployee searchEmployeeByNumber(const Employee table[], int sizeTable, long numberToFind);
+    PtrToEmployee searchEmployeeByName(const Employee table[], int sizeTable, char* nameToFind);
+    PtrToEmployee searchEmployeeByPhone(const Employee table[], int sizeTable, char* numberToFind);
+    PtrToEmployee searchEmployeeBySalary(const Employee table[], int sizeTable, double numberToFind);
+
+    extern Employee EmployeeTable[];
+    extern const int EmployeeTableEntries;
+
+    PtrToEmployee matchPtr;
+    matchPtr = searchEmployeeByNumber(EmployeeTable, EmployeeTableEntries, 1001l);
+
+    if(matchPtr != NULL){
+        printf("Employee ID 1001 is in record %ld\n", matchPtr - EmployeeTable);
+    }
+    else{
+        printf("Employee ID is NOT found in the record\n");
+    }
+
+    matchPtr = searchEmployeeByName(EmployeeTable, EmployeeTableEntries, "Tony Bobcat");
+    if(matchPtr != NULL){
+        printf("Employee Tony Bobcat is in record %ld\n", matchPtr - EmployeeTable);
+    }
+    else{
+        printf("Employee Tony Bobcat is NOT found in the record\n");
+    }
+
+    matchPtr = searchEmployeeByPhone(EmployeeTable, EmployeeTableEntries, "909-555-1235");
+    if(matchPtr != NULL){
+        printf("Employee Phone Number is in record %ld\n", matchPtr - EmployeeTable);
+    }else{
+        printf("Employee Phone Number is NOT found in the record\n");
+    }
+
+    matchPtr = searchEmployeeBySalary(EmployeeTable, EmployeeTableEntries, 6.34);
+    if(matchPtr != NULL){
+        printf("Employee Salary is in record %ld\n", matchPtr - EmployeeTable);
+    }else
+        printf("Employe Salary is NOT found in the record\n");
+    
+    return EXIT_SUCCESS;
+
+
+}
+
---
+++ employeeTable.c
@@ -0,0 +1,27 @@
+#include <string.h>
+#include <stdlib.h>
+#include "employee.h" //this is our struct that we just made.
+
+Employee EmployeeTable[] =
+{
+    //5 employees in employee table.
+    //wanna calculate how many employees we have cus ppl might be adding more in.
+    {1001l, "Daphne Borromeo", "909-555-2134", 8.78},
+    {1011l, "Tammy Franklin",  "213-555-1212", 4.50},
+    {1140l, "Forris Perl",     "310-555-1215", 7.80},
+    {4011l, "Tony Bobcat",     "909-555-1235", 6.34},
+    {5045l, "Brian Height",    "714-555-2749", 8.32}
+
+};
+
+const int EmployeeTableEntries = sizeof(EmployeeTable)/sizeof(EmployeeTable[0]);
+/*
+void main(){
+    printf("%ld\n",sizeof(EmployeeTable));
+    printf("%ld\n",sizeof(EmployeeTable[0]));
+    printf("%ld\n",sizeof(long));
+    printf("%ld\n",sizeof(char*)); 
+    printf("%ld\n",sizeof(char*));
+    printf("%ld\n",sizeof(double));
+
+}*/
